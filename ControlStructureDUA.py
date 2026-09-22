d10884230de89b070803bf9a5a9abdf8707d7b0c
sykllh <sykllhidmi@gmail.com>
2026-09-22 11:04:13 +0700

    membuat variabel

---
+++ ControlStructureDUA.py
@@ -0,0 +1,7 @@
+angka 1 = int(input("masukkan angka pertama"))
+angka 2 = int(input("masukkan angka kedua"))
+angka 3 = int(input("masukkan angka ketiga"))
+
+if angka 1 >= angka 2 AND angka 1 >= angka 3:
+    terbesar = angka 1
+elif angka 2 >= angka 1 AND angka 2
